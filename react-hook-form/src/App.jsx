cdc8100a722cbb0ff23a61c5362042e6c3ad7e9b
abrar-fahim-priom <priomshads@gmail.com>
2024-03-13 00:03:43 +0600

    first

---
+++ react-hook-form/src/App.jsx
@@ -0,0 +1,5 @@
+import LoginFrom from "./Components/Forms/LoginForm";
+
+export default function App() {
+  return <LoginFrom />;
+}
---
+++ react-hook-form/src/Components/Forms/LoginForm.jsx
@@ -0,0 +1,60 @@
+import { useForm } from "react-hook-form";
+import Field from "../Field";
+import FieldSet from "../FieldSet";
+
+export default function LoginFrom() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
+
+  return (
+    <form
+      className="flex flex-col justify-center items-center"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <FieldSet label="Enter Login Details">
+        <Field label="Email" error={errors.email}>
+          <input
+            {...register("email", { required: "Email is required" })}
+            className={`p-1 border border-box w-[300px] rounded-md ${
+              errors.email ? "border-red-500" : "border-gray-400"
+            } `}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Your Email"
+          />
+        </Field>
+        <Field label="Password" error={errors.password}>
+          <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Your password must be at least 8 characters",
+              },
+            })}
+            className={`p-1 border border-box w-[300px] rounded-md ${
+              errors.password ? "border-red-500" : "border-gray-400"
+            } `}
+            type="Password"
+            name="Password"
+            id="Password"
+            placeholder="Enter Password"
+          />
+        </Field>
+        <Field>
+          <button className="text-lg text-white cursor-pointer p-1 border rounded-lg bg-purple-400">
+            Login
+          </button>
+        </Field>
+      </FieldSet>
+    </form>
+  );
+}
